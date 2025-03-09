ab116204a76ba98592a166acb8d1c4c52ba7a6e2
Vitto Tedja <vitto.tedja2332@gmail.com>
2025-03-09 14:51:22 +0800

    add: mock backend

--- graffiti-backend/main.go
+++ graffiti-backend/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	r.Run(":8080")
 }
