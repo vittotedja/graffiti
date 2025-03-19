0ba5b0a63fd763089994ea05644908fe763c57b2
Davina Wijaya <phandavina@gmail.com>
2025-03-20 01:45:33 +0800

    add: random generator unitlity functions

---
+++ graffiti-backend/util/random.go
@@ -0,0 +1,47 @@
+package util
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt generates a random integer between min and max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString generates a random string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// RandomUsername generates a random username
+func RandomUsername() string {
+	return RandomString(6)
+}
+
+// RandomEmail generates a random email
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
+}
+
+// Random Fullname generates a random full name
+func RandomFullname() string {
+	return fmt.Sprintf("%s %s", RandomString(6), RandomString(6))
+}
