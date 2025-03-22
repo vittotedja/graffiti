c2aea64bd6c525e2decefe43a84654df22b32295
Vitto Tedja <vitto.tedja2332@gmail.com>
2025-03-23 00:26:41 +0800

    add: jwt function

---
+++ graffiti-backend/token/jwt_maker_test.go
@@ -0,0 +1,72 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/stretchr/testify/require"
+	"github.com/vittotedja/graffiti/graffiti-backend/util"
+)
+
+func TestJWTMaker(t *testing.T) {
+	maker, err := newJWTMaker(util.RandomString(24))
+	require.NoError(t, err)
+
+	username := util.RandomString(12)
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, payload, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	require.NotEmpty(t, payload)
+
+	payload, err = maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	require.NotEmpty(t, payload)
+
+	require.NotEmpty(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Minute)
+}
+
+func TestExpiredJWTToken(t *testing.T) {
+	maker, err := newJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomString(12)
+
+	token, payload, err := maker.CreateToken(username, -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	require.NotEmpty(t, payload)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+
+}
+
+func TestInvalidJWTTokenAlgNone(t *testing.T) {
+	payload, err := NewPayload(util.RandomString(12), time.Minute)
+	require.NoError(t, err)
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
+	token, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	require.NoError(t, err)
+
+	maker, err := newJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrInvalidToken.Error())
+	require.Nil(t, payload)
+
+}
