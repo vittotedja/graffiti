6b2ce040aa87546837f1985e6057c7f77fba8726
Davina Wijaya <phandavina@gmail.com>
2025-03-25 18:56:00 +0800

    add: new dev env using docker containers for server

--- graffiti-backend/util/config.go
+++ graffiti-backend/util/config.go
@@ -5,9 +5,10 @@ import (
 )
 
 type Config struct {
-	DBDriver            string        `mapstructure:"DB_DRIVER"`
-	DBSource            string        `mapstructure:"DB_SOURCE"`
-	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
+	Env           string `mapstructure:"ENV"`
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
