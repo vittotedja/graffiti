8d3b47a5ee9adc4ea19fc10786912e2201767ac9
Davina Wijaya <phandavina@gmail.com>
2025-03-27 20:38:53 +0800

    add: not so sensitive app env

--- graffiti-backend/util/config.go
+++ graffiti-backend/util/config.go
@@ -18,16 +18,16 @@ func LoadConfig(path string) (config Config, err error) {
 
 	viper.AutomaticEnv()
 
-	// err = viper.ReadInConfig()
-	// if err != nil {
-	// 	return
-	// }
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
 
-	if err := viper.ReadInConfig(); err != nil {
-        if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
-            return config, err
-        }
-    }
+	// if err := viper.ReadInConfig(); err != nil {
+    //     if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+    //         return config, err
+    //     }
+    // }
 
 	err = viper.Unmarshal(&config)
 	return
