ea38dd920a6585d85ca501e5a3b5542841c0f852
Vitto Surya Tedja <vitto.tedja2332@gmail.com>
2025-03-29 17:09:42 +0800

    feat: enhance logger with additional logging methods

--- graffiti-backend/util/logger/meta.go
+++ graffiti-backend/util/logger/meta.go
@@ -17,16 +17,24 @@ type Logger struct {
 	entry *logrus.Entry
 }
 
+// Info logs an info message
 func (l *Logger) Info(format string, args ...interface{}) {
 	l.entry.Infof(format, args...)
 }
 
+// Debug logs a debug message
 func (l *Logger) Debug(format string, args ...interface{}) {
 	l.entry.Debugf(format, args...)
 }
 
+// Error logs an error message with an actual error
 func (l *Logger) Error(msg string, err error) {
 	l.entry.WithError(err).Error(msg)
+}
+
+// Errorf logs a business error message
+func (l *Logger) Errorf(format string, args ...interface{}) {
+	l.entry.Errorf(format, args...)
 }
 
 func (m *Meta) GetLogger() *Logger {
