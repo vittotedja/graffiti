12e0f980c57688b8dc44cfc8793cbae4b26ec540
Vitto Surya Tedja <vitto.tedja2332@gmail.com>
2025-03-29 16:43:40 +0800

    feat: add request ID middleware for context enrichment

---
+++ graffiti-backend/util/logger/middleware.go
@@ -0,0 +1,26 @@
+package logger
+
+import (
+	"context"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+func Middleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		reqID := c.GetHeader("X-Request-ID")
+		if reqID == "" {
+			reqID = uuid.New().String()
+		}
+
+		meta := &Meta{
+			RequestID: reqID,
+		}
+
+		ctx := context.WithValue(c.Request.Context(), metadataKey, meta)
+		c.Request = c.Request.WithContext(ctx)
+
+		c.Next()
+	}
+}
