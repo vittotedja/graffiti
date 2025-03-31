e13c1b1f2928413c8c9efee8c781f2b968975328
Vitto Tedja <vitto.tedja2332@gmail.com>
2025-03-31 17:16:59 +0800

    add: random seed

--- graffiti-backend/db/migration/insert_users.go
+++ graffiti-backend/db/migration/insert_users.go
@@ -33,7 +33,7 @@ func main() {
 	semaphore := make(chan struct{}, maxConcurrency)
 	var wg sync.WaitGroup
 
-	rand.NewSource(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 
 	for i := 0; i < totalUsers; i++ {
 		wg.Add(1)
