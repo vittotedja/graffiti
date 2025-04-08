fbb48373fff0ba485f8df99c5295fe4418a9d30a
Vitto Surya Tedja <vitto.tedja2332@gmail.com>
2025-04-09 00:52:08 +0800

    refactor: update Redis client to use UniversalClient for better compatibility

--- graffiti-backend/util/redis/client.go
+++ graffiti-backend/util/redis/client.go
@@ -1,21 +1,31 @@
 package redis
 
 import (
+	"context"
 	"crypto/tls"
 	"github.com/redis/go-redis/v9"
 	"github.com/vittotedja/graffiti/graffiti-backend/util"
+	"log"
 )
 
-func NewRedisClient(cfg util.Config) *redis.Client {
-	opt := &redis.Options{
-		Addr: cfg.RedisHost,
-	}
-	if cfg.RedisAuth != "" {
-		opt.Password = cfg.RedisAuth
-	}
-	if cfg.RedisTLS {
-		opt.TLSConfig = &tls.Config{}
+func NewRedisClient(cfg util.Config) redis.UniversalClient {
+	opts := &redis.ClusterOptions{
+		Addrs: []string{cfg.RedisHost},
 	}
 
-	return redis.NewClient(opt)
+	if cfg.RedisTLS {
+		opts.TLSConfig = &tls.Config{}
+	}
+
+	if cfg.RedisAuth != "" {
+		opts.Password = cfg.RedisAuth
+	}
+
+	client := redis.NewClusterClient(opts)
+	err := client.Ping(context.Background())
+	if err != nil {
+		log.Fatalf("failed to connect to redis: %v at this address: %s", err, cfg.RedisHost)
+	}
+
+	return client
 }
